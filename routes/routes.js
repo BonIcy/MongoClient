8d51af08492544c740edb60a3fb7d1de8071ae4f
asynkDF <mrlgsus22@gmail.com>
2023-09-13 18:36:09 -0500

    docs: :memo: Comentarios

--- routes/routes.js
+++ routes/routes.js
@@ -21,7 +21,7 @@ router.get('/endpoint1', async (req, res) => {
         await client.connect();
         const db = client.db(nombreBase)
         const collection = db.collection('ingredientes');
-        const result = await collection.find({stock : { $lt : 400}}).toArray()
+        const result = await collection.find({stock : { $lt : 400}}).toArray() //$lt es un operador de comparación de mongo que significa less than (menor que)
         res.json(result);
         client.close
         } catch (error) {
@@ -63,7 +63,7 @@ router.put('/endpoint4', async (req, res) => {
         await client.connect();
         const db = client.db(nombreBase)
         const collection = db.collection('ingredientes');
-        const result = await collection.updateMany({}, { $inc : { precio: 1.5 }}).toArray()
+        const result = await collection.updateMany({}, { $inc : { precio: 1.5 }}).toArray() // $inc es un operador de actualización en mongo usado para incrementar (multiplicar)
         res.json(result);
         client.close
         } catch (error) {
@@ -91,7 +91,7 @@ router.get('/endpoint6', async (req, res) => {
         await client.connect();
         const db = client.db(nombreBase)
         const collection = db.collection('categorias');
-        const result = await collection.find({}, { nombre: 1, descripcion: 1 }).toArray()
+        const result = await collection.find({}, { nombre: 1, descripcion: 1 }).toArray() //primer argumento vacio del método find ({})  significa que se recuperaran todos los documentos de la colección
         res.json(result);
         client.close
         } catch (error) {
@@ -118,7 +118,7 @@ router.post('/endpoint8', async (req, res) => {
         const client = new MongoClient(bases);
         await client.connect();
         const db = client.db(nombreBase);
-        const nuevoIngrediente = {
+        const nuevoIngr = {
             nombre: 'Name',
             descripcion: 'Descripción',
             precio: 2.0,
@@ -127,7 +127,7 @@ router.post('/endpoint8', async (req, res) => {
         const collection = db.collection('ingredientes');
         const result = await collection.updateOne(
             { nombre: 'Clásica' },
-            { $push: { ingredientes: nuevoIngrediente } })
+            { $push: { ingredientes: nuevoIngr } }) //$push es un operador de actualización (mongo) hecho para agregar elementos a un array en un documento existente
         res.json(result);
         client.close
         } catch (error) {
@@ -157,7 +157,7 @@ router.get('/endpoint10', async (req, res) => {
         const collection = db.collection('Chefs');
         const result = await collection.updateOne(
             { nombre: 'ChefC' },
-            { $set: { especialidad: 'cocina Internacional' }} )
+            { $set: { especialidad: 'cocina Internacional' }} ) //$set se utiliza para cambiar o actualizar el valor de un campo especifico en un documento sin afectar otros campos ni reemplazar el documento completo
         res.json(result);
         client.close
         } catch (error) {
@@ -172,7 +172,7 @@ router.get('/endpoint11', async (req, res) => {
         await client.connect();
         const db = client.db(nombreBase);
         const collection = db.collection('ingredientes');
-        const result = await collection.find().sort({ precio: -1 }).limit(1).toArray();
+        const result = await collection.find().sort({ precio: -1 }).limit(1).toArray(); //limit es una función de consulta que se utiliza para limitar el número de documentos que se recuperan como resultado de una consulta, en este caso solo queremos recuperar 1 documento
         res.json(result[0]);
         client.close();
     } catch (error) {
@@ -187,7 +187,8 @@ router.get('/endpoint12', async (req, res) => {
         await client.connect();
         const db = client.db(nombreBase);
         const collection = db.collection('hamburguesas');
-        const result = await collection.find({ ingredientes: { $not: { $in: ['Queso cheddar'] } } }).toArray();
+        const result = await collection.find({ ingredientes: { $not: { $in: ['Queso cheddar'] } } }).toArray(); //not se usa para negar la expresion que esta en su interior
+        //$in es para buscar coincidencias con uno de los valores proporcionados por el array
         res.json(result);
         client.close();
     } catch (error) {
@@ -219,7 +220,7 @@ router.get('/endpoint14', async (req, res) => {
         await client.connect();
         const db = client.db(nombreBase);
         const collection = db.collection('ingredientes');
-        const result = await collection.find({ descripcion: { $regex: /clásico/i } }).toArray();
+        const result = await collection.find({ descripcion: { $regex: /clasico/i } }).toArray(); // $regex permite buscar documentos cuyo campo cumpla con una expresión regular específica (lo que esta dentro de los slash /clasico/). la 'i' al final es para omitir mayusculas o minusculas y que estas no afecten la busqueda
         res.json(result);
         client.close();
     } catch (error) {
@@ -234,7 +235,7 @@ router.get('/endpoint15', async (req, res) => {
         await client.connect();
         const db = client.db(nombreBase);
         const collection = db.collection('hamburguesas');
-        const result = await collection.find({ precio: { $lte: 9 } }).toArray();
+        const result = await collection.find({ precio: { $lte: 9 } }).toArray(); // $lte evalua si un valor en un campo es menor o igual al valor especificado
         res.json(result);
         client.close();
     } catch (error) {
@@ -249,7 +250,7 @@ router.get('/endpoint16', async (req, res) => {
         const db = client.db(nombreBase);
         const collection = db.collection('chefs');
         const result = await collection.countDocuments();
-        res.json({ count: result });
+        res.json({ count: result }); // literalmente es un contador XD
         client.close();
     } catch (error) {
         console.log(error.message);
@@ -263,7 +264,7 @@ router.get('/endpoint17', async (req, res) => {
         await client.connect();
         const db = client.db(nombreBase);
         const collection = db.collection('categorias');
-        const result = await collection.find({ descripcion: { $regex: /gourmet/i } }).toArray();
+        const result = await collection.find({ descripcion: { $regex: /gourmet/i } }).toArray(); // misma logica empleada que en el endpoint 14
         res.json(result);
         client.close();
     } catch (error) {
@@ -278,6 +279,9 @@ router.delete('/endpoint18', async (req, res) => {
         await client.connect();
         const db = client.db(nombreBase);
         const collection = db.collection('hamburguesas');
+        // $expr se usa para evaluar una condicion
+        // $size se utiliza para seleccionar documentos en función del tamaño de un campo de tipo array
+        //si ingredientes tiene menos de 5 elementos en la longitud  se eliminara esa hamburguesa
         const result = await collection.deleteMany({ $expr: { $lt: [{ $size: "$ingredientes" }, 5] } });
         res.json(result);
         client.close();
@@ -287,7 +291,7 @@ router.delete('/endpoint18', async (req, res) => {
 });
 
 
-router.post('/cendpoint19', async (req, res) => {
+router.post('/endpoint19', async (req, res) => {
     try {
         const client = new MongoClient(bases);
         await client.connect();
@@ -312,7 +316,7 @@ router.get('/endpoint20', async (req, res) => {
         await client.connect();
         const db = client.db(nombreBase);
         const collection = db.collection('hamburguesas');
-        const result = await collection.find().sort({ precio: 1 }).toArray();
+        const result = await collection.find().sort({ precio: 1 }).toArray();  // el sort ordena los resultados de acuerdo al precio en orden ascendente
         res.json(result);
         client.close();
     } catch (error) {
@@ -325,7 +329,7 @@ router.get('/endpoint21', async (req, res) => {
         await client.connect();
         const db = client.db(nombreBase);
         const collection = db.collection('ingredientes');
-        const result = await collection.find({ precio: { $gte: 2, $lte: 5 } }).toArray();
+        const result = await collection.find({ precio: { $gte: 2, $lte: 5 } }).toArray(); // buscamos ingredientes que su precio sea mayor o igual a 2 y menor o igual a 5, empleando $gte(mayor o igual) y $lte(menor o igual)
         res.json(result);
         client.close();
     } catch (error) {
@@ -342,7 +346,7 @@ router.put('/endpoint22', async (req, res) => {
         const collection = db.collection('ingredientes');
         const result = await collection.updateOne(
             { nombre: 'Pan' },
-            { $set: { descripcion: 'Pan fresco y crujiente' } }
+            { $set: { descripcion: 'Pan fresco y crujiente' } } //utilizamos set nuevamente para actualizar o cambiar un dato especifico
         );
         res.json(result);
         client.close();
@@ -357,7 +361,7 @@ router.get('/endpoint23', async (req, res) => {
         await client.connect();
         const db = client.db(nombreBase);
         const collection = db.collection('hamburguesas');
-        const result = await collection.find({ ingredientes: { $in: ['Tomate', 'Lechuga'] } }).toArray();
+        const result = await collection.find({ ingredientes: { $in: ['Tomate', 'Lechuga'] } }).toArray(); // $in comprueba si al menos uno de los valores dichos se encuentra en el array
         res.json(result);
         client.close();
     } catch (error) {
@@ -371,7 +375,7 @@ router.get('/endpoint24', async (req, res) => {
         await client.connect();
         const db = client.db(nombreBase);
         const collection = db.collection('chefs');
-        const result = await collection.find({ nombre: { $ne: 'ChefA' } }).toArray();
+        const result = await collection.find({ nombre: { $ne: 'ChefA' } }).toArray(); //verifica que el nombre del chef sea diferente a ChefA, con $ne
         res.json(result);
         client.close();
     } catch (error) {
@@ -388,7 +392,7 @@ router.put('/endpoint25', async (req, res) => {
         const collection = db.collection('hamburguesas');
         const result = await collection.updateMany(
             { categoria: 'Gourmet' },
-            { $inc: { precio: 2 } }
+            { $inc: { precio: 2 } } //el primer argumento selecciona a los gourmet y el segundo actualiza su precio
         );
         res.json(result);
         client.close();
@@ -404,7 +408,7 @@ router.get('/endpoint26', async (req, res) => {
         await client.connect();
         const db = client.db(nombreBase);
         const collection = db.collection('ingredientes');
-        const result = await collection.find().sort({ nombre: 1 }).toArray();
+        const result = await collection.find().sort({ nombre: 1 }).toArray(); //ordenar por nombre (alfabeticament)
         res.json(result);
         client.close();
     } catch (error) {
@@ -419,7 +423,7 @@ router.get('/endpoint27', async (req, res) => {
         await client.connect();
         const db = client.db(nombreBase);
         const collection = db.collection('hamburguesas');
-        const result = await collection.find().sort({ precio: -1 }).limit(1).toArray();
+        const result = await collection.find().sort({ precio: -1 }).limit(1).toArray(); //ordena las hamburguesas de manera descendente (-1), y el limit solo deja obtener la primera, o sea la mas cara
         res.json(result[0]);
         client.close();
     } catch (error) {
@@ -436,7 +440,7 @@ router.post('/endpoint28', async (req, res) => {
         const collection = db.collection('hamburguesas');
         const result = await collection.updateMany(
             { categoria: 'Clásica' },
-            { $addToSet: { ingredientes: 'Pepinillos' } }
+            { $addToSet: { ingredientes: 'Pepinillos' } } // $addToSet es un operador de actualización  que se utiliza para agregar un elemento a un array si ese elemento no existe ya en ese array. En este caso, se agrega pepinillos a los ingredientes de las hamburguesas clasicas
         );
         res.json(result);
         client.close();
@@ -466,7 +470,7 @@ router.get('/endpoint30', async (req, res) => {
         await client.connect();
         const db = client.db(nombreBase);
         const collection = db.collection('hamburguesas');
-        const result = await collection.find({ ingredientes: { $size: 7 } }).toArray();
+        const result = await collection.find({ ingredientes: { $size: 7 } }).toArray(); //devuelve hamburguesas con justo 7 ingredientes
         res.json(result);
         client.close();
     } catch (error) {
@@ -480,10 +484,10 @@ router.get('/endpoint31', async (req, res) => {
         await client.connect();
         const db = client.db(nombreBase);
         const collection = db.collection('hamburguesas');
-        const result = await collection.aggregate([
-            { $match: { chef: 'ChefC', categoria: 'Gourmet' } },
-            { $sort: { precio: -1 } },
-            { $limit: 1 }
+        const result = await collection.aggregate([ // aggregate() hace agregacion (es un proceso que permite realizar transformaciones y operaciones complejas en los datos antes de devolverlos) dentro de hamburguesas, la agregacion tiene 3 etapas ({})
+            { $match: { chef: 'ChefC', categoria: 'Gourmet' } }, //se filtran los documentos que coinciden con el chef ChefC y la categoría gourmet
+            { $sort: { precio: -1 } }, //se ordenan las hamburguesas restantes en orden descendente según el precio
+            { $limit: 1 } //deja la mas cara unicamente
         ]).toArray();
         res.json(result[0]);
         client.close();
@@ -499,8 +503,8 @@ router.get('/endpoint32', async (req, res) => {
         const db = client.db(nombreBase);
         const collection = db.collection('hamburguesas');
         const result = await collection.aggregate([
-            { $unwind: '$ingredientes' },
-            { $group: { _id: '$ingredientes', count: { $sum: 1 } } }
+            { $unwind: '$ingredientes' }, // $unwind separa los datos de un array, en este caso deja cada ingrediente individual
+            { $group: { _id: '$ingredientes', count: { $sum: 1 } } } // $group agrupa los ingredientes (por el id) y posteriormente count cuenta cuantos documentos hay en el grupo y suma 1 por cada uno
         ]).toArray();
         res.json(result);
         client.close();
@@ -516,7 +520,7 @@ router.get('/endpoint33', async (req, res) => {
         const db = client.db(nombreBase);
         const collection = db.collection('hamburguesas');
         const result = await collection.aggregate([
-            { $group: { _id: '$chef', count: { $sum: 1 } } }
+            { $group: { _id: '$chef', count: { $sum: 1 } } } //agrupamos los chefs y posteriormente añadimos un contador que suma 1 por cada chef en el grupo
         ]).toArray();
         res.json(result);
         client.close();
@@ -532,7 +536,7 @@ router.get('/endpoint34', async (req, res) => {
         const db = client.db(nombreBase);
         const collection = db.collection('hamburguesas');
         const result = await collection.aggregate([
-            { $group: { _id: '$categoria', count: { $sum: 1 } } },
+            { $group: { _id: '$categoria', count: { $sum: 1 } } }, //categoria con mayor cantidad de hamburguesas
             { $sort: { count: -1 } },
             { $limit: 1 }
         ]).toArray();
@@ -551,11 +555,11 @@ router.get('/endpoint35', async (req, res) => {
         const collection = db.collection('hamburguesas');
         const result = await collection.aggregate([
             { $unwind: '$ingredientes' },
-            { $lookup: { from: 'ingredientes', localField: 'ingredientes', foreignField: 'nombre', as: 'ingredientesData' } },
+            { $lookup: { from: 'ingredientes', localField: 'ingredientes', foreignField: 'nombre', as: 'datos' } }, // con $lookup se realiza una busqueda de nombres de ingredientes y las almacena en datos
             {
                 $group: {
                     _id: '$chef',
-                    costoTotal: { $sum: { $sum: '$ingredientesData.precio' } }
+                    costoTotal: { $sum: { $sum: '$datos.precio' } } //agrupa los chefs y los precios totales sumados de los datos anteriores
                 }
             }
         ]).toArray();
@@ -571,7 +575,7 @@ router.get('/endpoint36', async (req, res) => {
         await client.connect();
         const db = client.db(nombreBase);
         const hamburguesas = await db.collection('hamburguesas').distinct('ingredientes');
-        const result = await db.collection('ingredientes').find({ nombre: { $nin: hamburguesas } }).toArray();
+        const result = await db.collection('ingredientes').find({ nombre: { $nin: hamburguesas } }).toArray(); //$nin singinfica que no coincide con ningun valor, en este caso devuelve los ingredientes que no esten en alguna hamburguesa
         res.json(result);
         client.close();
     } catch (error) {
@@ -587,7 +591,7 @@ router.get('/endpoint37', async (req, res) => {
         const collection = db.collection('hamburguesas');
         const result = await collection.aggregate([
             { $lookup: { from: 'categorias', localField: 'categoria', foreignField: 'nombre', as: 'categoriaData' } },
-            { $project: { _id: 0, nombre: 1, 'categoriaData.descripcion': 1 } }
+            { $project: { _id: 0, nombre: 1, 'categoriaData.descripcion': 1 } } //se especifica qué campos se incluirán o excluirán en el resultado , 0 para excluir y 1 para incluir
         ]).toArray();
         res.json(result);
         client.close();
@@ -607,7 +611,7 @@ router.get('/endpoint38', async (req, res) => {
             { $unwind: '$ingredientes' },
             { $group: { _id: '$chef', ingredientesCount: { $sum: 1 } } },
             { $sort: { ingredientesCount: -1 } },
-            { $limit: 1 }
+            { $limit: 1 } //chef con mayor cantidad de ingredientes total
         ]).toArray();
         res.json(result[0]);
         client.close();
@@ -623,7 +627,7 @@ router.get('/endpoint39', async (req, res) => {
         await client.connect();
         const db = client.db(nombreBase);
         const result = await db.collection('hamburguesas').aggregate([
-            { $group: { _id: '$categoria', precioPromedio: { $avg: '$precio' } } }
+            { $group: { _id: '$categoria', precioPromedio: { $avg: '$precio' } } } // $avg para calcular promedio
         ]).toArray();
         res.json(result);
         client.close();
@@ -641,7 +645,7 @@ router.get('/endpoint40', async (req, res) => {
         const result = await db.collection('hamburguesas').aggregate([
             { $group: { _id: '$chef', hamburguesaCara: { $max: '$precio' } } },
             { $lookup: { from: 'chefs', localField: '_id', foreignField: 'nombre', as: 'chefData' } },
-            { $project: { _id: 0, 'chefData.nombre': 1, hamburguesaCara: 1 } }
+            { $project: { _id: 0, 'chefData.nombre': 1, hamburguesaCara: 1 } } // se muestra el nombre del chef y la hamburguesa con precio mayor
         ]).toArray();
         res.json(result);
         client.close();
