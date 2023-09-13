bf671fc3ab9c1433e6d9aeae4152e8b406c9f7bf
asynkDF <mrlgsus22@gmail.com>
2023-09-13 13:49:06 -0500

    test: :white_check_mark: 40 endpoints testeados
    finalmente :D (Documentacion proximamente)

--- routes/routes.js
+++ routes/routes.js
@@ -319,5 +319,335 @@ router.get('/endpoint20', async (req, res) => {
         console.log(error.message);
     }
 });
+router.get('/endpoint21', async (req, res) => {
+    try {
+        const client = new MongoClient(bases);
+        await client.connect();
+        const db = client.db(nombreBase);
+        const collection = db.collection('ingredientes');
+        const result = await collection.find({ precio: { $gte: 2, $lte: 5 } }).toArray();
+        res.json(result);
+        client.close();
+    } catch (error) {
+        console.log(error.message);
+    }
+});
+
+
+router.put('/endpoint22', async (req, res) => {
+    try {
+        const client = new MongoClient(bases);
+        await client.connect();
+        const db = client.db(nombreBase);
+        const collection = db.collection('ingredientes');
+        const result = await collection.updateOne(
+            { nombre: 'Pan' },
+            { $set: { descripcion: 'Pan fresco y crujiente' } }
+        );
+        res.json(result);
+        client.close();
+    } catch (error) {
+        console.log(error.message);
+    }
+});
+//23
+router.get('/endpoint23', async (req, res) => {
+    try {
+        const client = new MongoClient(bases);
+        await client.connect();
+        const db = client.db(nombreBase);
+        const collection = db.collection('hamburguesas');
+        const result = await collection.find({ ingredientes: { $in: ['Tomate', 'Lechuga'] } }).toArray();
+        res.json(result);
+        client.close();
+    } catch (error) {
+        console.log(error.message);
+    }
+});
+
+router.get('/endpoint24', async (req, res) => {
+    try {
+        const client = new MongoClient(bases);
+        await client.connect();
+        const db = client.db(nombreBase);
+        const collection = db.collection('chefs');
+        const result = await collection.find({ nombre: { $ne: 'ChefA' } }).toArray();
+        res.json(result);
+        client.close();
+    } catch (error) {
+        console.log(error.message);
+    }
+});
+
+
+router.put('/endpoint25', async (req, res) => {
+    try {
+        const client = new MongoClient(bases);
+        await client.connect();
+        const db = client.db(nombreBase);
+        const collection = db.collection('hamburguesas');
+        const result = await collection.updateMany(
+            { categoria: 'Gourmet' },
+            { $inc: { precio: 2 } }
+        );
+        res.json(result);
+        client.close();
+    } catch (error) {
+        console.log(error.message);
+    }
+});
+
+
+router.get('/endpoint26', async (req, res) => {
+    try {
+        const client = new MongoClient(bases);
+        await client.connect();
+        const db = client.db(nombreBase);
+        const collection = db.collection('ingredientes');
+        const result = await collection.find().sort({ nombre: 1 }).toArray();
+        res.json(result);
+        client.close();
+    } catch (error) {
+        console.log(error.message);
+    }
+});
+
+
+router.get('/endpoint27', async (req, res) => {
+    try {
+        const client = new MongoClient(bases);
+        await client.connect();
+        const db = client.db(nombreBase);
+        const collection = db.collection('hamburguesas');
+        const result = await collection.find().sort({ precio: -1 }).limit(1).toArray();
+        res.json(result[0]);
+        client.close();
+    } catch (error) {
+        console.log(error.message);
+    }
+});
+
+
+router.post('/endpoint28', async (req, res) => {
+    try {
+        const client = new MongoClient(bases);
+        await client.connect();
+        const db = client.db(nombreBase);
+        const collection = db.collection('hamburguesas');
+        const result = await collection.updateMany(
+            { categoria: 'Clásica' },
+            { $addToSet: { ingredientes: 'Pepinillos' } }
+        );
+        res.json(result);
+        client.close();
+    } catch (error) {
+        console.log(error.message);
+    }
+});
+
+router.delete('/endpoint29', async (req, res) => {
+    try {
+        const client = new MongoClient(bases);
+        await client.connect();
+        const db = client.db(nombreBase);
+        const collection = db.collection('chefs');
+        const result = await collection.deleteMany({ especialidad: 'Cocina Vegetariana' });
+        res.json(result);
+        client.close();
+    } catch (error) {
+        console.log(error.message);
+    }
+});
+
+
+router.get('/endpoint30', async (req, res) => {
+    try {
+        const client = new MongoClient(bases);
+        await client.connect();
+        const db = client.db(nombreBase);
+        const collection = db.collection('hamburguesas');
+        const result = await collection.find({ ingredientes: { $size: 7 } }).toArray();
+        res.json(result);
+        client.close();
+    } catch (error) {
+        console.log(error.message);
+    }
+});
+
+router.get('/endpoint31', async (req, res) => {
+    try {
+        const client = new MongoClient(bases);
+        await client.connect();
+        const db = client.db(nombreBase);
+        const collection = db.collection('hamburguesas');
+        const result = await collection.aggregate([
+            { $match: { chef: 'ChefC', categoria: 'Gourmet' } },
+            { $sort: { precio: -1 } },
+            { $limit: 1 }
+        ]).toArray();
+        res.json(result[0]);
+        client.close();
+    } catch (error) {
+        console.log(error.message);
+    }
+});
+
+router.get('/endpoint32', async (req, res) => {
+    try {
+        const client = new MongoClient(bases);
+        await client.connect();
+        const db = client.db(nombreBase);
+        const collection = db.collection('hamburguesas');
+        const result = await collection.aggregate([
+            { $unwind: '$ingredientes' },
+            { $group: { _id: '$ingredientes', count: { $sum: 1 } } }
+        ]).toArray();
+        res.json(result);
+        client.close();
+    } catch (error) {
+        console.log(error.message);
+    }
+});
+
+router.get('/endpoint33', async (req, res) => {
+    try {
+        const client = new MongoClient(bases);
+        await client.connect();
+        const db = client.db(nombreBase);
+        const collection = db.collection('hamburguesas');
+        const result = await collection.aggregate([
+            { $group: { _id: '$chef', count: { $sum: 1 } } }
+        ]).toArray();
+        res.json(result);
+        client.close();
+    } catch (error) {
+        console.log(error.message);
+    }
+});
+
+router.get('/endpoint34', async (req, res) => {
+    try {
+        const client = new MongoClient(bases);
+        await client.connect();
+        const db = client.db(nombreBase);
+        const collection = db.collection('hamburguesas');
+        const result = await collection.aggregate([
+            { $group: { _id: '$categoria', count: { $sum: 1 } } },
+            { $sort: { count: -1 } },
+            { $limit: 1 }
+        ]).toArray();
+        res.json(result[0]);
+        client.close();
+    } catch (error) {
+        console.log(error.message);
+    }
+});
+
+router.get('/endpoint35', async (req, res) => {
+    try {
+        const client = new MongoClient(bases);
+        await client.connect();
+        const db = client.db(nombreBase);
+        const collection = db.collection('hamburguesas');
+        const result = await collection.aggregate([
+            { $unwind: '$ingredientes' },
+            { $lookup: { from: 'ingredientes', localField: 'ingredientes', foreignField: 'nombre', as: 'ingredientesData' } },
+            {
+                $group: {
+                    _id: '$chef',
+                    costoTotal: { $sum: { $sum: '$ingredientesData.precio' } }
+                }
+            }
+        ]).toArray();
+        res.json(result);
+        client.close();
+    } catch (error) {
+        console.log(error.message);
+    }
+});
+router.get('/endpoint36', async (req, res) => {
+    try {
+        const client = new MongoClient(bases);
+        await client.connect();
+        const db = client.db(nombreBase);
+        const hamburguesas = await db.collection('hamburguesas').distinct('ingredientes');
+        const result = await db.collection('ingredientes').find({ nombre: { $nin: hamburguesas } }).toArray();
+        res.json(result);
+        client.close();
+    } catch (error) {
+        console.log(error.message);
+    }
+});
+
+router.get('/endpoint37', async (req, res) => {
+    try {
+        const client = new MongoClient(bases);
+        await client.connect();
+        const db = client.db(nombreBase);
+        const collection = db.collection('hamburguesas');
+        const result = await collection.aggregate([
+            { $lookup: { from: 'categorias', localField: 'categoria', foreignField: 'nombre', as: 'categoriaData' } },
+            { $project: { _id: 0, nombre: 1, 'categoriaData.descripcion': 1 } }
+        ]).toArray();
+        res.json(result);
+        client.close();
+    } catch (error) {
+        console.log(error.message);
+    }
+});
+
+
+router.get('/endpoint38', async (req, res) => {
+    try {
+        const client = new MongoClient(bases);
+        await client.connect();
+        const db = client.db(nombreBase);
+        const collection = db.collection('hamburguesas');
+        const result = await collection.aggregate([
+            { $unwind: '$ingredientes' },
+            { $group: { _id: '$chef', ingredientesCount: { $sum: 1 } } },
+            { $sort: { ingredientesCount: -1 } },
+            { $limit: 1 }
+        ]).toArray();
+        res.json(result[0]);
+        client.close();
+    } catch (error) {
+        console.log(error.message);
+    }
+});
+
+
+router.get('/endpoint39', async (req, res) => {
+    try {
+        const client = new MongoClient(bases);
+        await client.connect();
+        const db = client.db(nombreBase);
+        const result = await db.collection('hamburguesas').aggregate([
+            { $group: { _id: '$categoria', precioPromedio: { $avg: '$precio' } } }
+        ]).toArray();
+        res.json(result);
+        client.close();
+    } catch (error) {
+        console.log(error.message);
+    }
+});
+
+
+router.get('/endpoint40', async (req, res) => {
+    try {
+        const client = new MongoClient(bases);
+        await client.connect();
+        const db = client.db(nombreBase);
+        const result = await db.collection('hamburguesas').aggregate([
+            { $group: { _id: '$chef', hamburguesaCara: { $max: '$precio' } } },
+            { $lookup: { from: 'chefs', localField: '_id', foreignField: 'nombre', as: 'chefData' } },
+            { $project: { _id: 0, 'chefData.nombre': 1, hamburguesaCara: 1 } }
+        ]).toArray();
+        res.json(result);
+        client.close();
+    } catch (error) {
+        console.log(error.message);
+    }
+});
 
 module.exports = router;
